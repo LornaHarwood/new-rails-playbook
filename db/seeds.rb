ef22e949e5cf7cb2a9e0e377f2ddab3247d8cdfd
Lorna <lorna@dxw.com>
2020-06-08 13:01:01 +0100

    Create seed for index page
    This is to ensure that there is data within the database during
    development.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ Page.create(title: 'index', content: '<h1>index</h1>', slug: 'index')
