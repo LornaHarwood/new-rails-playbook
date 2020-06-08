ec7537ab40b85babc222027b10daa208ffb63322
Lorna <lorna@dxw.com>
2020-06-08 15:39:38 +0100

    Install and run standardrb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
- Page.create(title: 'index', content: '<h1>index</h1>', slug: 'index')
+Page.create(title: "index", content: "<h1>index</h1>", slug: "index")
