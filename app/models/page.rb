baff1a37df3b1ad522962bc8377c7adb2e403d2e
Lorna <lorna@dxw.com>
2020-07-03 14:15:44 +0100

    Set slug conditions for validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,7 @@
 class Page < ApplicationRecord
   validates_presence_of :content, :slug
+  validates :slug, format: {without: /[^\w-]/,
+                            message: "only supports alphanumeric and - characters"}
 
   def to_param
     "#{id}-#{slug}"
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -6,5 +6,29 @@ RSpec.describe Page, type: :model do
   describe "validations" do
     it { should validate_presence_of(:content) }
     it { should validate_presence_of(:slug) }
+
+    context "When a slug contains a space" do
+      it "is not valid" do
+        expect(Page.new).not_to allow_value("the is a bad slug").for(:slug)
+      end
+    end
+
+    context "When a slug contains a question mark" do
+      it "is not valid" do
+        expect(Page.new).not_to allow_value("slug??").for(:slug)
+      end
+    end
+
+    context "When a slug only contains word characters" do
+      it "is valid" do
+        expect(Page.new).to allow_value("thisisaslug").for(:slug)
+      end
+    end
+
+    context "When a slug contains dashes" do
+      it "is valid" do
+        expect(Page.new).to allow_value("this-is-a-slug").for(:slug)
+      end
+    end
   end
 end
