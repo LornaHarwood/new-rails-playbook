b7443f9de745ddc98c01e172aaa66c9fa3e5d0e9
Lorna <lorna@dxw.com>
2020-07-29 13:45:17 +0100

    Create a sidebar on index page to list all pages
    I have made adjustments to the body and main styling to ensure this
    sidebar sits properly within the page. This will require me in later
    commits to make amendments to other action pages which require this
    styling.

--- spec/features/user_can_navigate_to_specific_page_from_index_page_spec.rb
+++ spec/features/user_can_navigate_to_specific_page_from_index_page_spec.rb
@@ -2,14 +2,16 @@ require "rails_helper"
 
 RSpec.feature "User can navigate to a specific page from the index page" do
   context "When multiple pages exist" do
-    scenario "the index page's show links target the associated page" do
+    scenario "the page title links to the associated page_path" do
       Page.create(title: "test title one", content: "test content one", slug: "test-slug-one")
       test_page_two = Page.create(title: "test title two", content: "test content two", slug: "test-slug-two")
       Page.create(title: "test title three", content: "test content three", slug: "test-slug-three")
 
       visit pages_path
 
-      find('input[type="submit"][data-slug="test-slug-two"][data-action="show"]').click
+      within ".sidebar-nav" do
+        find('a[href$="test-slug-two"]').click
+      end
 
       expect(page).to have_content(test_page_two.title)
       expect(page).to have_content(test_page_two.content)
